1c932b12d40555396e1dad7d334dc103dfe434f4
afifahpn <55918778+afifahpn@users.noreply.github.com>
2022-09-18 22:40:56 +0700

    belajar flutter

---
+++ lib/font-style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+TextStyle mainHeader =
+    TextStyle(fontSize: 26, color: Colors.blueGrey, fontFamily: 'Quicksand');
+
+TextStyle subHeader =
+    TextStyle(fontSize: 16, color: Colors.black87, fontFamily: 'Quicksand');
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:belajarflutter/font-style.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        title: 'Homey',
         theme: ThemeData(
           // This is the theme of your application.
           //
@@ -24,6 +26,39 @@ class MyApp extends StatelessWidget {
           // is not restarted.
           primarySwatch: Colors.blue,
         ),
-        home: Scaffold(body: Text("Flutter Pertama")));
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text(
+                "Homey",
+              ),
+              backgroundColor: Colors.blueGrey,
+              actions: [
+                // IconButton( onPressed: (() {}), icon: Icon(Icons.mail)) //(){} agar ada animasi splash
+              ],
+            ),
+            body: SafeArea(
+                child: Container(
+              margin:
+                  EdgeInsets.only(left: 30.0, top: 0, right: 30.0, bottom: 0),
+              // EdgeInsets.all(20),
+              padding: EdgeInsets.only(left: 0, top: 5.0, right: 0, bottom: 0),
+              // didalam colum tidak ada margin dan padding, hanya ada di container
+              child: Column(children: [
+                Image(
+                  image: AssetImage('assets/images/clean2.jpg'),
+                  height: 500,
+                ),
+                Text(
+                  "Feel Like Home",
+                  style: mainHeader,
+                ),
+                Text(
+                  'Enjoy the sensation',
+                  style: subHeader,
+                )
+              ]),
+            )
+                // Icon(Icons.mail), //safe area agar tidak nabrak dengan navbar
+                )));
   }
 }
